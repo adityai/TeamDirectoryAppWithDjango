6d0e51e71d1b8fed6d6bd9d46bb65cb7b870d72b
Aditya Inapurapu <adityaii@gmail.com>
2016-11-06 15:59:54 -0800

    Added details and edit page to allow users to edit their details.

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/adityai/code/TeamDirectoryAppWithDjango/venv/bin/python3
+#!/Users/adityai/code/TeamDirectoryAppWithDjango/venv/bin/python3.5
 from django.core import management
 
 if __name__ == "__main__":
